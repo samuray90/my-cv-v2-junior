93e36d9f18eea8ba09e18a47766643ac9996bb47
Mihaela Chetran <mihaela.chetran@yahoo.com>
2020-04-01 17:17:32 +0100

    let's start with the about me section

--- gulp/sass.js
+++ gulp/sass.js
@@ -56,10 +56,10 @@ const sass = ({
           path.join(dir.source, dir.component)
         ]
       }))
-      .pipe(plugins.notify({
-        title: 'Pug Starter - CodeTap',
-        message: 'Converting (sass|scss) into beautiful CSS'
-      }))
+      // .pipe(plugins.notify({
+      //   title: 'Pug Starter - CodeTap',
+      //   message: 'Converting (sass|scss) into beautiful CSS'
+      // }))
       .on('error', function(error) {
         plugins.sass.logError;
         browserSync.notify(printError(error), 25000);
